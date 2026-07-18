8d909a33750b0b5a99b11731ce5209b4d98d1f42
JM_Handsome_DOG <zmc13012271892@163.com>
2026-07-18 22:10:23 +0800

    77

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -10,7 +10,7 @@ module.exports = (env, argv) => {
     resolve: { extensions: ['.ts', '.tsx', '.js', '.jsx'] },
     module: {
       rules: [
-        { test: /\.tsx?$/, use: 'ts-loader', exclude: /node_modules/ },
+        { test: /\.tsx?$/, use: { loader: 'ts-loader', options: { configFile: path.resolve(__dirname, 'tsconfig.json') } }, exclude: /node_modules/ },
         { test: /\.css$/, use: ['style-loader', 'css-loader'] },
         { test: /\.(png|svg|jpg|jpeg|gif)$/, type: 'asset/resource' },
       ],
